b2c7c7b3c7c0e0ed4dc1224a074ccf4bc047bea0
Tobias <63608600+formmaterialobject@users.noreply.github.com>
2025-01-21 21:46:47 +0100

    Add files via upload

---
+++ variablen.js
@@ -0,0 +1,16 @@
+/*
+Variablen sind im Grunde Speicherplätze für Daten in einem Programm. 
+In JavaScript haben wir seit ECMAScript 6 (ES6) hauptsächlich 
+const und let zur Deklaration. 
+
+Variables are in first place storage locations for data 
+within a program. In JavaScript, since ECMAScript 6 (ES6), 
+we primarily use const and let for declaration."
+*/
+
+let vorname = "Tobias";
+vorname = "Max";
+console.log(vorname) //Max
+console.log(`My name is ${vorname}`)
+
+
