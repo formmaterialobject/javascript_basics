e89bf661a42c6486163e135faf33c0ea139a473d
Tobias <63608600+formmaterialobject@users.noreply.github.com>
2025-01-22 15:34:19 +0100

    Add files via upload

---
+++ statement_vs_expression.js
@@ -0,0 +1,16 @@
+/*
+Anweisung / Statement, tut etwas (führt eine Aktionen aus)
+Ausdruck / Expression geben einen Wert zrück - erzeugen immer einen Wert. 
+
+Statement, do something (execute actions)
+Expression return a value - always generate a value. 
+
+
+*/
+lotto = [0, 45, 12, 4, 34, 23];
+// statement -->  It declares a variable named lotto and assigns an array of numbers to it
+numone = lotto[2];
+// The entire line numone = lotto[2]; is an assignment statement
+// lotto[2] is an expression that evaluates to the value at index 2 of the lotto array  
+console.log(numone); 
+// It calls the console.log function to print the value of the variable numone to the console --> statement 
